aae07aaa084cb00465dfd63e6df4968ef4e33bed
Tomotaka Hirajoh <tomotaka.hirajoh@gmail.com>
2018-12-20 02:03:51 +0900

    動いた

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@
 #Route::get('hello/{id?}/{pass?}', 'HelloController@index');
 Route::get('hello', 'HelloController@index');
 Route::get('hello/other', 'HelloController@other');
-
+Route::get('/', function () {
+    return view('welcome');
+});
